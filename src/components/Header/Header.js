3421fa420517adf9265059e61d4ba1f125d9b0d7
Janis Costadelli <77967942+janiscostadelli@users.noreply.github.com>
2021-07-13 11:08:57 -0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { MainContainer, StyledButton, Logo } from "./styled";
 import { useHistory } from "react-router-dom";
 import { logout } from '../../services/user';
-import logo from "../../assets/Logo.png";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   const history = useHistory();
